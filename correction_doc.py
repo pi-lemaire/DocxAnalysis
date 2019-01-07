ed541f6002979b4f4d7dce2ab209a08b8a2394be
pi-lemaire <36866395+pi-lemaire@users.noreply.github.com>
2019-01-07 16:06:21 +0100

    added batch processing support

--- correction_doc.py
+++ correction_doc.py
@@ -2,6 +2,9 @@
 from docx import Document
 from lxml import etree
 import sys
+import glob, os
+import io
+from path import Path
 
 def introspection(variable):
   L = dir(variable)
@@ -121,55 +124,103 @@ def checkAltText(images):
   for im in images:
     print (im._inline.docPr.get("descr"))
 
+
+
+
+
 styleNames = ["Heading 1", "Description", "Title", "Heading 2", "Heading 3"]
-filename = sys.argv[1]
-f = open(filename, 'rb')
-document = Document(f)
 
-styles = document.styles
-paragraphs = document.paragraphs
-images = document.inline_shapes
+analyzeDir = sys.argv[1]
 
-# print("---")
-# checkStyles(styles, styleNames)
-# print("---")
-# checkEmptyParagraphs(paragraphs)
-# print("---")
-# print("Taux d'application des styles:")
-# checkStylesApplied(paragraphs)
-# print("---")
-# print("Numéros de pages + table des matieres : à vérifier dans le docx")
-# print("---")
-# print("Texte remplacement premiere photo=" + str(images[0]._inline.docPr.get("descr")))
-# print("Texte remplacement toutes photos")
-# checkAltText(images)
+for f in Path(analyzeDir).walkfiles():
+    if not (f.ext==".docx" or f.ext==".DOCX"):
+        continue
+    
+    
+    print("===================")
+    
+    #currStudentName=os.path.basename(os.path.normpath(f.dirname()))
+    #currStudentName = currStudentName[0:currStudentName.find('_')]
+    foldersPath = os.path.normpath(f.dirname())
+    lastSlash = foldersPath.rfind("/")
+    prevSlash = foldersPath[0:lastSlash-1].rfind("/")
+    studentFolder = foldersPath[prevSlash+1:lastSlash-1]
+    print(studentFolder[0:studentFolder.find('_')])
+    
+    print("----")
+    
+    #continue
+    
+    filename = f.abspath()
+    
+        
+    print(filename)
+    
+    print("-----")
+
+    #filename = sys.argv[1]
+    f = open(filename, 'rb')
+    
+    
+    try:
+        document = Document(f)
+    except:
+        print("######## ERREUR : ouverture document impossible")
+        continue
+    
+
+    
+
+    styles = document.styles
+    paragraphs = document.paragraphs
+    images = document.inline_shapes
+
+    # print("---")
+    # checkStyles(styles, styleNames)
+    # print("---")
+    # checkEmptyParagraphs(paragraphs)
+    # print("---")
+    # print("Taux d'application des styles:")
+    # checkStylesApplied(paragraphs)
+    # print("---")
+    # print("Numéros de pages + table des matieres : à vérifier dans le docx")
+    # print("---")
+    # print("Texte remplacement premiere photo=" + str(images[0]._inline.docPr.get("descr")))
+    # print("Texte remplacement toutes photos")
+    # checkAltText(images)
 
 
-cTitle,cTitleTotal = textToStyle(paragraphs, "Title", ["Livre de recettes"])
-cHeading3, cHeading3Total = textToStyle(paragraphs, "Heading 3", [u"Ingrédients", u"Réalisation"])
-cRecap, cRecapTotal = textToStyle(paragraphs, "Description", [u"Préparation\u00A0:"])
-alt = images[0]._inline.docPr.get("descr")
+    try:
+        cTitle,cTitleTotal = textToStyle(paragraphs, "Title", ["Livre de recettes"])
+        cHeading3, cHeading3Total = textToStyle(paragraphs, "Heading 3", [u"Ingrédients", u"Réalisation"])
+        cRecap, cRecapTotal = textToStyle(paragraphs, "Description", [u"Préparation\u00A0:"])
+        alt = images[0]._inline.docPr.get("descr")
 
+        if (int(cTitleTotal) ==0):
+          print("W1. Probleme detecte, verifier le docx")
+        else:
+          print("W1 Appl Titre. " + str(cTitle/cTitleTotal*100) +"% (" + str(int(cTitleTotal)) + "/" + str(int(cTitle)) + ")")
+        print("W2 nom prenom. : verif docx")
+        print("W3. ")
+        checkEmptyParagraphs(paragraphs)
+        print("W4 saut 1ere page. verif docx")
+        if (int(cHeading3Total) ==0):
+          print("W5. Probleme detecte, verifier le docx")
+        else:
+          print("W5 Appl Titre 3. " + str(cHeading3/cHeading3Total*100) +"% (" + str(int(cHeading3)) + "/" + str(int(cHeading3Total)) + ")")
+        print("W6 7 8 10. " + str(checkStyles(styles, styleNames)))
+        if (int(cRecapTotal) ==0):
+          print("W9. Probleme detecte, verifier le docx")
+        else:
+          print("W9 Appl Descr. " + str(cRecap/cRecapTotal*100) +"% (" + str(int(cRecap)) + "/" + str(int(cRecapTotal)) + ")")
+        print("W10. Saut de page recette/type plat : voir recap styles plus haut")
+        print("W11 numero page. : verif docx")
+        print("W12 tdm. verif docx ")
+        print("W13 alt." + ("None",alt)[alt != None])
+    except:
+        print("######## ERREUR : le script n'a pas pu tourner normalement sur ce document")
+        continue
+    
+    f.close()
+    
 
-if (int(cTitleTotal) ==0):
-  print("W1. Probleme detecte, verifier le docx")
-else:
-  print("W1 Appl Titre. " + str(cTitle/cTitleTotal*100) +"% (" + str(int(cTitleTotal)) + "/" + str(int(cTitle)) + ")")
-print("W2 nom prenom. : verif docx")
-print("W3. ")
-checkEmptyParagraphs(paragraphs)
-print("W4 saut 1ere page. verif docx")
-if (int(cHeading3Total) ==0):
-  print("W5. Probleme detecte, verifier le docx")
-else:
-  print("W5 Appl Titre 3. " + str(cHeading3/cHeading3Total*100) +"% (" + str(int(cHeading3)) + "/" + str(int(cHeading3Total)) + ")")
-print("W6 7 8 10. " + str(checkStyles(styles, styleNames)))
-if (int(cRecapTotal) ==0):
-  print("W9. Probleme detecte, verifier le docx")
-else:
-  print("W9 Appl Descr. " + str(cRecap/cRecapTotal*100) +"% (" + str(int(cRecap)) + "/" + str(int(cRecapTotal)) + ")")
-print("W10. Saut de page recette/type plat : voir recap styles plus haut")
-print("W11 numero page. : verif docx")
-print("W12 tdm. verif docx ")
-print("W13 alt." + ("None",alt)[alt != None])
-f.close()
